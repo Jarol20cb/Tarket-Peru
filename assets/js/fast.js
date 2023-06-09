1c65e21cb47ea282382a035d20566a7c7816fa0d
Jarol20cb <118091274+Jarol20cb@users.noreply.github.com>
2023-06-09 16:35:42 -0500

    Add files via upload

---
+++ assets/js/fast.js
@@ -0,0 +1,6 @@
+// Inicializar SmoothScroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 5000, // Velocidad del desplazamiento en milisegundos
+    easing: 'easeInOutQuart' // Función de aceleración
+  });
+  
