857a9da19b0fb8a75174d93950f5d57510953adc
Jarol20cb <118091274+Jarol20cb@users.noreply.github.com>
2023-06-11 11:08:07 -0500

    Update meyawo.js

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,25 +1,22 @@
-
-// smooth scroll
-$(document).ready(function(){
-    $(".navbar .nav-link").on('click', function(event) {
-
-        if (this.hash !== "") {
-
-            event.preventDefault();
-
-            var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
-                window.location.hash = hash;
-            });
-        } 
+$(document).ready(function() {
+    $(".custom-navbar .link").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 700, function(){
+          window.location.hash = hash;
+        });
+        // Cerrar el menú desplegable si está abierto
+        $('#nav-toggle').removeClass('is-active');
+        $('.custom-navbar .nav').removeClass('show');
+      }
     });
-});
-
-// navbar toggle
-$('#nav-toggle').click(function(){
-    $(this).toggleClass('is-active')
-    $('ul.nav').toggleClass('show');
-});
+  
+    $('#nav-toggle').click(function(){
+      $(this).toggleClass('is-active')
+      $('.custom-navbar .nav').toggleClass('show');
+    });
+  });
+  
